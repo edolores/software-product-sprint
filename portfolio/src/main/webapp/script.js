dd1e5cc5b7bfe50ea7e1f41d42d967ad3ce6e969
ErickDolores <edolores@sps-program.com>
2020-03-08 06:29:25 -0400

    Add to Misoya Info Window

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -714,18 +714,14 @@ function createMap6(){
     var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h6 id="firstHeading" class="firstHeading"><b>Misoya</b></h6>'+
+      '<h7 id="firstHeading" class="firstHeading"><b>Misoya</b></h7>'+
       '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
+      '<p>Misoya is the first ramen restaurant ' +
+      'I\'ve ever visited. I came to New York on a vacation '+
+      'trip with my friends and they\'d learned that I\'d never had ramen '+
+      'before. So, naturally, we took an excursion from Central Park '+
+      'to Misoya, a place my friend had been to before. '+
+      'To this day, Misoya is one of my favorite ramen restaurants.</p>'+
       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
       '(last visited June 22, 2009).</p>'+
